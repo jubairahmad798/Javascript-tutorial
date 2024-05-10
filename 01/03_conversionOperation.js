bd09039ed886a3885e28522331c4bb24c9a58bd4
jubairahmad798 <144090148+jubairahmad798@users.noreply.github.com>
2024-05-10 09:01:17 +0000

    conversion of datatypes

---
+++ 01/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let score=33
+
+// let score=null
+
+//  let score="33abc"
+
+// let score=undefined
+
+// let score=true
+// let score="Jubair"
+
+
+//Note- "33"=33
+// "33abc"=NaN
+//true=1,false=0
+//"Jubair"=NaN
+
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// // console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn=1
+// let isLoggedIn="jubair"
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1==true
+//0==false
+//""=false
+// "jubair"=true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
