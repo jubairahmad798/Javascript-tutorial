daa225b616734c378ed537c6551095dddceeb56a
jubairahmad798 <144090148+jubairahmad798@users.noreply.github.com>
2024-05-11 16:48:16 +0000

    comaparison operator

---
+++ 01/04_comparison.js
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log("2">1)
+
+console.log(null>0);//null to number
+console.log(null==0); //js not convert null to number //equLITY check different work from >=,>,<
+console.log(null>=0);//null to number
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<=0)
+
+
+//===
+console.log("2"===2) //strictly checks also checks its dataTYpe
